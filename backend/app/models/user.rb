e585b716d71869f2ec2d81f6087348582636c91a
Jade Lynn Masker <jadelynn@masker.family>
2021-02-03 23:17:52 -0500

    added validations to users

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,23 @@
 class User < ApplicationRecord
+  validates(
+    :username,
+    format: {
+      with: /\A[\w\-]{3,21}\z/,
+      message: 'Username must be between 3 and 21 word or number characters.'
+    }
+  )
+  validates(
+    :password,
+    format: {
+      with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9])[\S]{8,21}\z/,
+      message: 'Password must be between 8 and 21 characters and must have one upper and lowercase letter and one number.'
+    }
+  )
+  validates(
+    :email,
+    format: {
+      with: /\A\S+@\S+\.\S+\a/,
+      message: 'Email must be valid.'
+    }
+  )
 end
