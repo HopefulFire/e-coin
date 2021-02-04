de09a958465432487c4b278897a4b8f450541bb2
Jade Lynn Masker <jadelynn@masker.family>
2021-02-04 15:54:38 -0500

    changed messages

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -6,14 +6,14 @@ class User < ApplicationRecord
     :username,
     format: {
       with: /\A[\w\-]{3,21}\z/,
-      message: 'Username must be between 3 and 21 word or number characters.'
+      message: 'must be between 3 and 21 word or number characters.'
     }
   )
   validates(
     :email_address,
     format: {
       with: /\A\S+@\S+\.\S+\a/,
-      message: 'Email must be valid.'
+      message: 'must be valid.'
     }
   )
 end
