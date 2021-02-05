1600d446789603e8e855144e9c0892c036038270
Jade Lynn Masker <jadelynn@masker.family>
2021-02-04 21:10:55 -0500

    added a missing do

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  scope :api, defaults: {format: :json}
+  scope :api, defaults: {format: :json} do
     devise_for(
       :users,
       controllers: { sessions: :sessions },
