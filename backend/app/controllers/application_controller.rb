73fa3bbfa044a6f60ffc5acdf83686c385ab3722
Jade Lynn Masker <jadelynn@masker.family>
2021-02-05 10:55:23 -0500

    Revert "add another missing bang"
    This reverts commit 2e98cd06a31b47fdcdfc378374e7c947c694325b.

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::API
   respond_to :json
   before_action :underscore_params!
   before_action :configure_permitted_parameters, if: :devise_controller?
-  before_action :authenticate_user!
+  before_action :authenticate_user
   
   private
   
