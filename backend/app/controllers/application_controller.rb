dcd581be45cc25ad6717a2507ff5b869c26f910a
Jade Lynn Masker <jadelynn@masker.family>
2021-02-04 19:27:45 -0500

    created a authenticate_user method in application controller

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -9,6 +9,20 @@ class ApplicationController < ActionController::API
     devise_parameter_sanitizer.permit(:sign_up, keys: %i[username])
   end
 
+  def authenticate_user
+    if request.headers['Authorization'].present?
+      authenticate_or_request_with_http_token do |token|
+        begin
+          jwt_payload = JWT.decode(token, Rails.application.secrets.secret_key_base).first
+
+          @current_user_id = jwt_payload['id']
+        rescue JWT::ExpiredSignature, JWT::VerificationError, JWT::DecodeError
+          head :unauthorized
+        end
+      end
+    end
+  end
+
   def underscore_params!
     params.deep_transform_keys!(&:underscore)
   end
