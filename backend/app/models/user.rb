aa72d05d905ab642f1baba12687ba6af0d56ee27
Jade Lynn Masker <jadelynn@masker.family>
2021-02-06 17:11:48 -0500

    added validations for users

--- backend/app/models/user.rb
+++ backend/app/models/user.rb
@@ -1,2 +1,6 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :username, presence: true, format: { with: /\A\w{3,21}\z/, message: 'only allows letters between 3 and 21 in length.' }
+  validates :email, presence: true, format: { with: /\A\S+@\S+\.\S+\z/, message: 'must be a valid email.' }
+  validates :password, presence: true, format: { with: /\A(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=\\S+$).{8, 21}\z/, message: 'must be a strong password.' }
 end
