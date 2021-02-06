8bbc5a070faf4eb0863c5b0b90c658fdfc95eaac
Jade Lynn Masker <jadelynn@masker.family>
2021-02-06 17:24:30 -0500

    scope users under api

--- backend/config/routes.rb
+++ backend/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :users
+  scope :api, defaults: { format: :json } do
+    resources :users
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
