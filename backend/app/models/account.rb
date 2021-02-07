a7ea4da91ea188923ca5ea8856176bce8a6c0c90
Jade Lynn Masker <jadelynn@masker.family>
2021-02-07 16:38:04 -0500

    added validation for account balance

--- backend/app/models/account.rb
+++ backend/app/models/account.rb
@@ -1,3 +1,11 @@
 class Account < ApplicationRecord
   belongs_to :user
+  validates(
+    :balance,
+    presence: true,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0
+    }
+  )
 end
