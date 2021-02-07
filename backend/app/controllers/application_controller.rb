ecb588d44a193983490e6848b2be53063b9ceba4
Jade Lynn Masker <jadelynn@masker.family>
2021-02-07 17:52:52 -0500

    remove jwt encode and decode to be moved elsewhere

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -1,14 +1,2 @@
 class ApplicationController < ActionController::API
-  private
-
-  def jwt_encode(payload)
-    JWT.encode(payload, Rails.application.secrets.secret_key_base, 'HS265')
-  end
-
-  def jwt_decode(token)
-    body, = JWT.decode(token, Rails.application.secrets.secret_key_base, true, algorithm: 'HS265')
-    HashWithIndifferentAccess.new(body)
-  rescue JWT::ExpiredSignature
-      nil    
-  end
 end
